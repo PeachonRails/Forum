501c74d116e29ba5f73448b05ec3c56de6abbaec
robo-hobo <robertzielinski@dobrapogoda24.pl>
2015-12-04 16:45:42 +0100

    Group relation in model, views from scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'groups#index'
   devise_for :users
+  resources :groups
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
