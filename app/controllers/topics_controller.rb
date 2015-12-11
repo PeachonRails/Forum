c1165b8d99f2b4fde99d06a1e9d77e2de25a14ef
robo-hobo <robertzielinski@dobrapogoda24.pl>
2015-12-11 15:05:59 +0100

    group's topics view

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    redirect_to group_path(:id => params[:group_id])
   end
 
   # GET /topics/1
