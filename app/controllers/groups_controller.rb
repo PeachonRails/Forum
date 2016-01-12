b58393e69b41a2470558fb12995da5dd88f0273e
mateuszskrok <m.p.skrok@gmail.com>
2016-01-12 23:58:29 +0100

    chyba z błedami

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,5 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
-  
 
   # GET /groups
   # GET /groups.json
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228222110) do
+ActiveRecord::Schema.define(version: 20160112224207) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20151228222110) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   create_table "topics", force: :cascade do |t|
